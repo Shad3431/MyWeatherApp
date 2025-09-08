8245163d3800cca8aca99ff63f217a51faa401ef
Shad3431 <sh.a.d3431@gmail.com>
2025-09-08 15:46:58 +0300

    fix Redux provider in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+
 import { LanguageProvider } from "./i18n/LanguageContext.jsx";
+import {store} from "./configStore/Store.js";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <LanguageProvider>
-            <App />
-        </LanguageProvider>
+        <Provider store={store}>
+            <LanguageProvider>
+                <App />
+            </LanguageProvider>
+        </Provider>
     </StrictMode>
 );
 
